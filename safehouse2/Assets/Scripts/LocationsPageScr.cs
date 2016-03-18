a13f8b63e66cdc1803963d1058f8f460fc550c80
patrick <promero@pdx.edu>
2016-03-17 17:16:12 -0700

    scene stuff

--- safehouse2/Assets/Scripts/LocationsPageScr.cs
+++ safehouse2/Assets/Scripts/LocationsPageScr.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class LocationsPageScr : MonoBehaviour {
+	
+	//public List<Place> places = new List<Place>();
+	public Agency parentAgency = null;
 
 	// Use this for initialization
 	void Start () {
-	
+		parentAgency = transform.parent.GetComponent<Agency> () as Agency;
 	}
 	
 	// Update is called once per frame
--- safehouse2/Assets/Scripts/ModList.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-class ModList : List<Modifier> {
-	public bool contains(string modstring) {
-		return Contains(new Modifier(modstring));
-	}
-}
-	
